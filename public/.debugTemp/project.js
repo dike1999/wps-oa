7d3890cd4e7f941a74178c56e4093ced8b396f1e
dike <1138832931@qq.com>
2023-08-01 00:33:24 +0800

    feat: update

--- public/.debugTemp/project.js
+++
@@ -1,1 +0,0 @@
-var projInfo = {"name":"wpstest","type":"wps"}
---
+++ wps-addon-build/precache-manifest.ef761076782b905e7aefb9b88224fc85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abfd7a8dab952ca739a944a0dff250f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "302931425c3a83719c40",
+    "url": "./static/css/main.0d44876c.chunk.css"
+  },
+  {
+    "revision": "f3e181df29909bc43dae",
+    "url": "./static/js/2.8e716880.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "./static/js/2.8e716880.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "302931425c3a83719c40",
+    "url": "./static/js/main.02bfdcda.chunk.js"
+  },
+  {
+    "revision": "ba66b4b16102fe8fc1c4",
+    "url": "./static/js/runtime-main.d77005e4.js"
+  }
+]);
